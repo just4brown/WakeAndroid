5f346ebe81fc31d33843369673a334f043a88b04
Brandon Brown <7200rpm@gmail.com>
2017-01-21 20:33:43 -0800

    Auth0 Native Login in Progress

--- app/src/main/java/com/lucerlabs/wake/LoginActivity.java
+++ app/src/main/java/com/lucerlabs/wake/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.lucerlabs.wake;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
@@ -16,46 +17,106 @@ import com.auth0.android.lock.Lock;
 import com.auth0.android.lock.LockCallback;
 import com.auth0.android.lock.utils.LockException;
 import com.auth0.android.result.Credentials;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
 
 public class LoginActivity extends AppCompatActivity {
 
-	private Lock mLock;
+    private Lock mLock;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client2;
 
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		Auth0 auth0 = new Auth0(getString(R.string.auth0_client_id), getString(R.string.auth0_domain));
-		mLock = Lock.newBuilder(auth0, mCallback)
-			//Add parameters to the builder
-			.build(this);
-		startActivity(mLock.newIntent(this));
-	}
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		// Your own Activity code
-		mLock.onDestroy(this);
-		mLock = null;
-	}
+        Auth0 auth0 = new Auth0(getString(R.string.auth0_client_id), getString(R.string.auth0_domain));
+        AuthenticationAPIClient client = new AuthenticationAPIClient(auth0);
 
-	private final LockCallback mCallback = new AuthenticationCallback() {
-		@Override
-		public void onAuthentication(Credentials credentials) {
-			Toast.makeText(getApplicationContext(), "Log In - Success", Toast.LENGTH_SHORT).show();
-			Intent mainApplicationIntent = new Intent(getApplicationContext(), MainActivity.class);
-			mainApplicationIntent.putExtra("AUTH_ID_TOKEN", credentials.getIdToken());
-			startActivity(mainApplicationIntent);
-			finish();
-		}
+        GoogleAuthProvider provider = new GoogleAuthProvider(getString(R.string.google_server_client_id), client);
+        //provider.setScopes(Arrays.asList(new Scope(Scopes.PLUS_ME), new Scope(Scopes.PLUS_LOGIN)));
+        //provider.setScopes(new Scope(DriveScopes.DRIVE_METADATA_READONLY));
+        //provider.setRequiredPermissions(new String[]{"android.permission.GET_ACCOUNTS"});
 
-		@Override
-		public void onCanceled() {
-			Toast.makeText(getApplicationContext(), "Log In - Cancelled", Toast.LENGTH_SHORT).show();
-		}
+        GoogleAuthHandler handler = new GoogleAuthHandler((provider));
 
-		@Override
-		public void onError(LockException error) {
-			Toast.makeText(getApplicationContext(), "Log In - Error Occurred", Toast.LENGTH_SHORT).show();
-		}
-	};
+        mLock = Lock.newBuilder(auth0, mCallback)
+                //Add parameters to the builder
+                .withAuthHandlers(handler)
+                .build(this);
+        startActivity(mLock.newIntent(this));
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client2 = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Your own Activity code
+        mLock.onDestroy(this);
+        mLock = null;
+    }
+
+    private final LockCallback mCallback = new AuthenticationCallback() {
+        @Override
+        public void onAuthentication(Credentials credentials) {
+            Toast.makeText(getApplicationContext(), "Log In - Success", Toast.LENGTH_SHORT).show();
+            Intent mainApplicationIntent = new Intent(getApplicationContext(), MainActivity.class);
+            mainApplicationIntent.putExtra("AUTH_ID_TOKEN", credentials.getIdToken());
+            startActivity(mainApplicationIntent);
+            finish();
+        }
+
+        @Override
+        public void onCanceled() {
+            Toast.makeText(getApplicationContext(), "Log In - Cancelled", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onError(LockException error) {
+            Toast.makeText(getApplicationContext(), "Log In - Error Occurred", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Login Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client2.connect();
+        AppIndex.AppIndexApi.start(client2, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client2, getIndexApiAction());
+        client2.disconnect();
+    }
 }
