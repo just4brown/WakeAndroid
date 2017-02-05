96fcacc0c726d589e780591c97be254c6f35372e
Justin Brown <justin@brownjt.com>
2017-02-04 19:32:28 -0800

    Fix sign out button listener assignment.

--- app/src/main/java/com/lucerlabs/wake/OnboardingFragment.java
+++ app/src/main/java/com/lucerlabs/wake/OnboardingFragment.java
@@ -2,10 +2,7 @@ package com.lucerlabs.wake;
 
 import android.app.Fragment;
 import android.content.Context;
-import android.databinding.ObservableArrayList;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,7 +31,7 @@ public class OnboardingFragment extends Fragment {
 		mSecondaryUserButton.setOnClickListener(mListener.getSecondaryUserSelectedListener());
 
 		mSignOutButton = (Button) mainView.findViewById(R.id.sign_out_button);
-		mSignOutButton.setOnClickListener(mListener.getSecondaryUserSelectedListener());
+		mSignOutButton.setOnClickListener(mListener.getSignOutButtonListener());
 		return mainView;
 	}
 
