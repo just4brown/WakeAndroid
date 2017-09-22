21d8fc4788b32211f75441c5b52581c48d8321c0
Brandon Brown <7200rpm@gmail.com>
2017-09-21 20:20:26 -0700

    More Progress

--- app/src/main/java/com/lucerlabs/wake/LoginActivity.java
+++ app/src/main/java/com/lucerlabs/wake/LoginActivity.java
@@ -1,150 +1,135 @@
 package com.lucerlabs.wake;
 
+import android.app.Dialog;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
-import com.auth0.android.Auth0;
-import com.auth0.android.authentication.AuthenticationAPIClient;
-import com.auth0.android.authentication.AuthenticationException;
-import com.auth0.android.callback.BaseCallback;
-import com.auth0.android.google.GoogleAuthHandler;
-import com.auth0.android.google.GoogleAuthProvider;
-import com.auth0.android.lock.AuthenticationCallback;
-import com.auth0.android.lock.Lock;
-import com.auth0.android.lock.LockCallback;
-import com.auth0.android.lock.utils.LockException;
-import com.auth0.android.result.Credentials;
-import com.auth0.android.result.UserProfile;
+
+
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import com.auth0.android.Auth0;
+import com.auth0.android.authentication.AuthenticationAPIClient;
+import com.auth0.android.authentication.AuthenticationException;
+import com.auth0.android.callback.BaseCallback;
+import com.auth0.android.provider.AuthCallback;
+import com.auth0.android.provider.WebAuthProvider;
+import com.auth0.android.result.Credentials;
+import com.auth0.android.result.UserProfile;
+
+
 public class LoginActivity extends AppCompatActivity {
 
-    private Lock mLock;
+    private Auth0 auth0;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
-    private GoogleApiClient client2;
-
-    private AuthenticationAPIClient client;
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        client2 = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        setContentView(R.layout.activity_login);
 
-        Auth0 auth0 = new Auth0(getString(R.string.auth0_client_id), getString(R.string.auth0_domain));
-        client = new AuthenticationAPIClient(auth0);
+        auth0 = new Auth0(this);
+        auth0.setOIDCConformant(true);
 
-        GoogleAuthProvider provider = new GoogleAuthProvider(getString(R.string.google_server_client_id), client);
-        GoogleAuthHandler handler = new GoogleAuthHandler((provider));
+        final Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+                                           @Override
+                                           public void onClick(View v) {
+                                               doLogin();
+                                           }
+                                       }
+        );
 
-        mLock = Lock.newBuilder(auth0, mCallback)
-            .withAuthHandlers(handler)
-            .withScope("offline_access openid")
-            .build(this);
+
 
         String accessToken = CredentialsManager.getCredentials(this).getAccessToken();
         if (accessToken == null) {
-            startActivity(mLock.newIntent(this));
             return;
         }
 
-        AuthenticationAPIClient aClient = new AuthenticationAPIClient(auth0);
-        aClient.userInfo(accessToken)
-            .start(new BaseCallback<UserProfile, AuthenticationException>() {
-                @Override
-                public void onSuccess(final UserProfile payload) {
-                    runOnUiThread(new Runnable() {
-                        public void run() {
-                            Toast.makeText(LoginActivity.this, "Automatic Login Success", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                    finish();
-                }
+        // If the token exists, try to fetch user profile
+        loginButton.setEnabled(false);
 
+        AuthenticationAPIClient authClient = new AuthenticationAPIClient(auth0);
+
+        authClient.userInfo(accessToken)
+                .start(new BaseCallback<UserProfile, AuthenticationException>() {
+                    @Override
+                    public void onSuccess(final UserProfile payload) {
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                Toast.makeText(LoginActivity.this, "Automatic Login Success", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        finish();
+                    }
+
+                    @Override
+                    public void onFailure(AuthenticationException error) {
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                Toast.makeText(LoginActivity.this, "Session Expired, please Log In", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        CredentialsManager.deleteCredentials(LoginActivity.this);
+                    }
+                });
+
+    }
+
+    private void doLogin() {
+        WebAuthProvider.init(auth0)
+                .withScheme("https")
+                .withAudience(String.format("https://%s/userinfo", getString(R.string.com_auth0_domain)))
+                .withScope("openid offline_access profile")
+                .start(this, authCallback);
+    }
+
+    private final AuthCallback authCallback = new AuthCallback() {
+        @Override
+        public void onFailure(@NonNull final Dialog dialog) {
+            runOnUiThread(new Runnable() {
                 @Override
-                public void onFailure(AuthenticationException error) {
-                    runOnUiThread(new Runnable() {
-                        public void run() {
-                            Toast.makeText(LoginActivity.this, "Session Expired, please Log In", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                    CredentialsManager.deleteCredentials(LoginActivity.this);
-                    startActivity(mLock.newIntent(LoginActivity.this));
+                public void run() {
+                    dialog.show();
                 }
             });
+        }
 
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // Your own Activity code
-        mLock.onDestroy(this);
-        mLock = null;
-    }
-
-    private final LockCallback mCallback = new AuthenticationCallback() {
         @Override
-        public void onAuthentication(Credentials credentials) {
-            CredentialsManager.saveCredentials(getApplicationContext(), credentials);
+        public void onFailure(AuthenticationException exception) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(LoginActivity.this, "Log In - Error Occurred", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public void onSuccess(@NonNull Credentials credentials) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(LoginActivity.this, "Log In - Success", Toast.LENGTH_SHORT).show();
+                }
+            });
+            CredentialsManager.saveCredentials(LoginActivity.this, credentials);
             startActivity(new Intent(LoginActivity.this, MainActivity.class));
             finish();
-        }
-
-        @Override
-        public void onCanceled() {
-            Toast.makeText(getApplicationContext(), "Log In - Cancelled", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onError(LockException error) {
-            Toast.makeText(getApplicationContext(), "Log In - Error Occurred", Toast.LENGTH_SHORT).show();
         }
     };
 
 
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Login Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client2.connect();
-        AppIndex.AppIndexApi.start(client2, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client2, getIndexApiAction());
-        client2.disconnect();
-    }
 }
